9f6ef62b3126ab5e94184449bad6f155a51e6469
Kevin Lee <everserv1111@gmail.com>
2021-11-20 21:18:21 -0500

    Updated Code

---
+++ public/locations/koreaScript.js
@@ -0,0 +1,19 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var imageObj = new Image();
+imageObj.onload = function(){
+    context.drawImage(imageObj, 100, 0, 547.2, 591.3);
+    context.scale(2, 2)
+};
+imageObj.src = "koreaMap.jpg"
+
+canvas.onmousemove = function(e){
+  let rgbColor = context.getImageData(e.layerX, e.layerY, 1, 1).data;
+  let rgbString = "rgb(" + rgbColor[0] + "," + rgbColor[1] + "," + rgbColor[2] + ")";
+  document.body.style.backgroundColor = rgbString;
+  let luma = 0.2126 * rgbColor[0] + 0.7152 * rgbColor[1] + 0.0722 * rgbColor[2];
+  document.querySelector("#visibility").innerHTML = `${((1 - (luma/256)) * 100).toFixed(1)}%`
+  document.querySelector("#color").innerHTML = rgbString;
+  document.querySelector(".title-text").style.color = rgbString;
+  document
+}
--- public/script.js
+++ public/script.js
@@ -1,19 +1,39 @@
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-var imageObj = new Image();
-imageObj.onload = function(){
-    context.drawImage(imageObj, 100, 0, 547.2, 591.3);
-    context.scale(2, 2)
-};
-imageObj.src = "Map.jpg"
+let cursor = document.getElementsByClassName("cursor");
+let plotTwist = document.getElementsByClassName("plotTwist");
 
-canvas.onmousemove = function(e){
-  let rgbColor = context.getImageData(e.layerX, e.layerY, 1, 1).data;
-  let rgbString = "rgb(" + rgbColor[0] + "," + rgbColor[1] + "," + rgbColor[2] + ")";
-  document.body.style.backgroundColor = rgbString;
-  let luma = 0.2126 * rgbColor[0] + 0.7152 * rgbColor[1] + 0.0722 * rgbColor[2];
-  document.querySelector("#visibility").innerHTML = `${((1 - (luma/256)) * 100).toFixed(1)}%`
-  document.querySelector("#color").innerHTML = rgbString;
-  document.querySelector(".title-text").style.color = rgbString;
-  document
+let plotTwists = ["Astro", "revelation"]
+
+
+for(let i = 0; i < cursor.length; i++){
+  let cursorOn = true;
+  setInterval(function(){
+    if(cursorOn){
+      (cursor[i]).innerHTML = "";
+    } else{ (cursor[i]).innerHTML = "|";}
+    cursorOn = !cursorOn
+  }, 500)
 }
+
+for(let i = 0; i < plotTwist.length; i++){
+  setTimeout(function(){
+    for(let j = 0; j < (plotTwist[i]).innerHTML.length; j++){
+      setTimeout(function(){
+        (plotTwist[i]).innerHTML = (plotTwist[i]).innerHTML.slice(0, -1)
+      }, 300 * j)
+    }
+    setTimeout(function(){
+      for(let j = 0; j < plotTwists[i].length; j++){
+        setTimeout(function(){
+          (plotTwist[i]).innerHTML += plotTwists[i][j];
+        }, 200 * j)
+      }
+      setTimeout(function(){
+        cursor[i].parentNode.removeChild(cursor[i])
+      }, 200 * (plotTwists[i].length - 1))
+    }, (plotTwist[i].innerHTML.length - 1) * 300)
+  }, 3000)
+}
+
+
+
+
